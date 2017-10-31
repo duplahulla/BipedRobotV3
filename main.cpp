133ada01f988436332534fb0d0a07d3f71871e10
Tamas Feher <fehertamas11@gmail.com>
2017-10-31 21:34:25 +0200

    Add main cpp

---
+++ main.cpp
@@ -0,0 +1,50 @@
+/*
+ * BipedRobotMotorControlSoftware.cpp
+ *
+ * Created: 11/16/2016 8:52:36 PM
+ * Author : Tamas
+ */ 
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <stdlib.h>
+//#include <avr/interrupt.h>
+#include <stdio.h>
+//#include "PID.h"
+//#include "math.h"
+//#include "wire.h"
+#include <util/delay.h>
+#include "USART.h"
+/********************************************************************************
+Macros and Defines
+********************************************************************************/
+
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
+
+//-----TWI Communication protocol-----
+#define TWI_BUFFER_SIZE 13
+/********************************************************************************
+Function Prototypes
+********************************************************************************/
+void setMotor(const float &value,const int &motor);
+void receiveCommand(int howMany);
+void slavesRespond();
+	//-------Global variables-----------------
+
+int main(void)
+{
+//unsigned char Mcustatus=MCUSR;
+MCUSR=0x00;
+//-------USART Initializing---------------
+usart_init ( MYUBRR ); // fire up the usart
+usart_putchar('D');
+//sprintf(out_str,"MCU status: %x\n", Mcustatus & 0xff);
+//usart_pstr(out_str);
+    while (1) 
+    {
+		//setMotor(20,2);
+		_delay_ms(10);
+		usart_putchar('K');
+    }
+}
+

d07f778eab9154c635e01c48aef1769dbfc44989
Tamas Feher <fehertamas11@gmail.com>
2018-01-17 09:48:12 +0100

    Added WORKING IIC protocol, but not register based.

--- main.cpp
+++ main.cpp
@@ -20,14 +20,19 @@
 Macros and Defines
 ********************************************************************************/
 
-#define BAUD 9600
+#define BAUD 115200
 #define MYUBRR F_CPU/16/BAUD-1
 #define ADC_A_Isense 0x00
 #define ADC_A_Psense 0x02
 #define ADC_B_Isense 0x01
 #define ADC_B_Psense 0x03
+
+#define I2C_A_Psense_address 0x00
+#define I2C_B_Psense_address 0x02
+#define I2C_A_Isense_address 0x04
+#define I2C_B_Isense_address 0x06
+
 //-----TWI Communication protocol-----
-#define TWI_BUFFER_SIZE 13
 #define SLAVE_ADRS	0x40
 /********************************************************************************
 Function Prototypes
@@ -43,103 +48,94 @@ int main(void){
 	//Timer0
 	PORTD &=~(1<<PIND6);
 	PORTD &=~(1<<PIND5);
-	DDRD |= (1 << DDD5);										// PB1 kimenetként való konfigurálása
+	DDRD |= (1 << DDD5);										// PD5-6 as output
 	DDRD |= (1 << DDD6);
 	OCR0A = 0xFF;
 	OCR0B = 0xFF-50;
-	TCCR0A |= (1<<COM0A1);										// Nem invertáló mód beállítása
+	TCCR0A |= (1<<COM0A1);										// Set non inverting mode
 	TCCR0A |= (1<<COM0B1);
-	TCCR0A |= (1 << WGM02) | (1 << WGM00);						// 10 bites fázis korrigált PWM beállítása
+	TCCR0A |= (1 << WGM02) | (1 << WGM00);						// 10 bit phase corriged PWM mode
 	
 	TIMSK0 |= ( 1 << TOIE0);					//Overflow interrupt enable
 	//Timer1
 	PORTB &=~(1<<PINB2);
 	PORTB &=~(1<<PINB1);
-	DDRB |= (1 << DDB1);										// PB1 kimenetként való konfigurálása
+	DDRB |= (1 << DDB1);										// PB1-2 as output
 	DDRB |= (1 << DDB2);
-	OCR1A = 0;
+	OCR1A = 0;													// set Duty cycle to 0
 	OCR1B = 0;
-	TCNT1=0xFF;
-	TCCR1A |= (1 << COM1A1);									// Nem invertáló mód beállítása
+	TCNT1=0xFF;													// set phase shift relative to Timer 0
+	TCCR1A |= (1 << COM1A1);									// Non inverting mode
 	TCCR1A |= (1 << COM1B1);
-	TCCR1A |= (1 << WGM10);										// 8 bites gyors PWM beállítása
-	//TCCR1B |= (1 << WGM13);
-	TCCR0B |= (1 << CS11) ;//| (1 << CS10);						// elõosztó beállítása 8 ra, és pwm indítása
-	TCCR1B |= (1 << CS11) ;//| (1 << CS10);										// elõosztó beállítása 8 ra, és pwm indítása
-	TIMSK1 |= (1 << TOIE1);										// Overflow interrupt enable
-	
+	TCCR1A |= (1 << WGM10);										// 8 bit phase corriged PWM mode
+    TIMSK1 |= (1 << TOIE1);										// Overflow interrupt enable
+	TCCR0B |= (1 << CS11); //| (1 << CS10);						// Set prescaler to 8 and start PWM on timer0
+	TCCR1B |= (1 << CS11); //| (1 << CS10);						// Set prescaler to 8 and start PWM on timer1
+
+	//-------IIC Initializing------------------
+	sei(); 				// Enable interrupts.
+	I2C_init( SLAVE_ADRS );		// Initialize TWI hardware for Slave operation.
 	
 	//-------ADC setup-----------------
-	DDRD |= (1 << DDD2);
-	PORTD &=~(1<<PIND2);
 	ADMUX=0x00;
 	ADCSRA |= (1<<ADATE);					//Enable auto triggering
 	ADCSRB |= (1<<ADTS2);					//Timer/Counter0 Overflow trigger
 	ADCSRA |= (1<<ADEN);					// ADEN: Set to turn on ADC , by default it is turned off
 	ADCSRA |= (1<<ADPS2)| (1<<ADPS1);			//set to make division factor 128
 	ADCSRA |= (1<<ADIE);					//ADIE: Interrupt enable
-	sei();
-//unsigned char Mcustatus=MCUSR;
+	//sei();
+unsigned char Mcustatus=MCUSR;
 MCUSR=0x00;
 	//-------USART Initializing---------------
-	usart_init ( MYUBRR ); // fire up the usart
-	usart_putchar('D');
+	//usart_init ( MYUBRR ); // fire up the usart
+	//usart_putchar('D');
 	//sprintf(out_str,"MCU status: %x\n", Mcustatus & 0xff);
 	//usart_pstr(out_str);
-	
-	//-------IIC Initializing------------------
-	
-	twiSlaveInit( SLAVE_ADRS );		// Initialize TWI hardware for Slave operation.	
-	sei(); 				// Enable interrupts.
-	twiSlaveEnable();				// Enable the TWI interface to receive data.
-	uint8_t *var=twiGetRxBuffer();
+	DDRC &=~(1<<PINC4);
+	DDRC &=~(1<<PINC5);
+	PORTC &=~(1<<PINC4);
+	PORTC &=~(1<<PINC4);
+	DDRD |= (1 << DDD1);
     while (1) 
     {
-		//setMotor(20,2);
-		
+		_delay_us(1);
+		PORTD &=~(1<<PIND1);
+		_delay_us(1);
+		PORTD |=(1<<PIND1);
 		//usart_putchar('K');
     }
 }
 
 ISR(ADC_vect){
-
-	PORTD &=~(1<<PIND2);
 	switch(ADMUX){
+	
 		 case ADC_A_Isense :
-		 measurementBuffer[0]=ADCL;
-		 measurementBuffer[1]=ADCH;
-		 ADMUX=ADC_A_Psense;
-		 ADCSRA &= ~(1<<ADATE);					//Disable auto trigger
-		 ADCSRA |= (1<<ADSC);
-		 PORTD  |=(1<<PIND2);
+			 I2C_writeRegister(I2C_A_Isense_address,ADCL,ADCH);
+			 ADMUX=ADC_A_Psense;
+			 ADCSRA &= ~(1<<ADATE);					//Disable auto trigger
+			 ADCSRA |= (1<<ADSC);
 		 break;
 		 case ADC_A_Psense :
-		 measurementBuffer[2]=ADCL;
-		 measurementBuffer[3]=ADCH;
-		 ADMUX=ADC_B_Isense;
-		 ADCSRA |= (1<<ADATE);					//Enable auto trigger
-		 ADCSRB &= ~(1<<ADTS1);
-		 ADCSRB |= (1<<ADTS2);					//Timer/Counter0 Overflow trigger
+			 I2C_writeRegister(I2C_A_Psense_address,ADCL,ADCH);
+			 ADMUX=ADC_B_Isense;
+			 ADCSRA |= (1<<ADATE);					//Enable auto trigger
+			 ADCSRB &= ~(1<<ADTS1);
+			 ADCSRB |= (1<<ADTS2);					//Timer/Counter0 Overflow trigger
 		 break;
 		 case ADC_B_Isense :
-		 measurementBuffer[4]=ADCL;
-		 measurementBuffer[5]=ADCH;
-		 ADMUX=ADC_B_Psense;
-		 ADCSRA &= ~(1<<ADATE);					//Disable auto trigger
-		 ADCSRA |= (1<<ADSC);
-		 PORTD  |=(1<<PIND2);
+			 I2C_writeRegister(I2C_B_Isense_address,ADCL,ADCH);
+			 ADMUX=ADC_B_Psense;
+			 ADCSRA &= ~(1<<ADATE);					//Disable auto trigger
+			 ADCSRA |= (1<<ADSC);
 		 break;
 		 case ADC_B_Psense :
-		 measurementBuffer[6]=ADCL;
-		 measurementBuffer[7]=ADCH;
-		 ADMUX=ADC_A_Isense;
-		 ADCSRA |= (1<<ADATE);					//Enable auto trigger
-		 ADCSRB |= (1<<ADTS1);
-		 ADCSRB |= (1<<ADTS2);					//Timer/Counter1 Overflow trigger
-		 twiSetTxBuffer(measurementBuffer);
+			 I2C_writeRegister(I2C_B_Psense_address,ADCL,ADCH);
+			 ADMUX=ADC_A_Isense;
+			 ADCSRA |= (1<<ADATE);					//Enable auto trigger
+			 ADCSRB |= (1<<ADTS1);
+			 ADCSRB |= (1<<ADTS2);					//Timer/Counter1 Overflow trigger
 		 break;
 	}
-	//usart_pstr("ADC /n");
 }
 ISR(TIMER0_OVF_vect)
 {
--- twiSlave.cpp
+++ twiSlave.cpp
@@ -1,63 +1,14 @@
-/*
- * The MIT License (MIT)
- * 
- * Copyright (c) 2016 Nels D. "Chip" Pearson (aka CmdrZin)
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * 
- * twiSlave.c
- *
- * Created: 8/10/2015	0.01	ndp
- *  Author: Chip
- * revision: 1/29/2016	0.02	ndp	 Add TxBuf[] test to support single read example.
- *
- * Based on the Atmel App Note AVR311 and enhanced to support FIFO data buffers for 
- * input and output.
- *
- * This I2C driver is interrupt driven and uses data FIFOs for input and output buffering.
- * The ISR type for the interrupt support routine causes C to automatically setup the interrupt vector.
- *
- * twiSlaveInit( adrs )			Set up TWI hardware and set Slave I2C Address.
- * twiSlaveEnable()				Enable I2C Slave interface.
- *
- * twiTransmitByte( data )		Place data into output buffer.
- *
- * twiReceiveByte()				Read data from input buffer.
- * twiDataInReceiveBuffer()		Check for available data in input buffer. This function should return 
- *								TRUE before calling twiReceiveByte() to get data.
- * twiDataInTransmitBuffer()	Check for empty TxBuff[]. This function should return FALSE when a new
- *								Read request is received to show that all of the prior data has been sent.
- *								If the buffer is not empty, call twiClearOutput() to recover.
- * twiClearOutput()				Reset the output buffer to empty. Used recover from sync errors.
- *
- * twiStuffRxBuf( data )		Allows manual input into input buffer for testing.
- * 
- */ 
-
 #include <avr/io.h>
+#include <util/twi.h>
 #include <avr/interrupt.h>
 
 #include "twiSlave.h"
 #include "USART.h"
-void flushTwiBuffers( void );
+volatile uint8_t buffer_address;
+volatile uint8_t txbuffer[0xFF];
+volatile uint8_t rxbuffer[0xFF];
 
-/* *** Local Defines *** */
+
 
 // TWI Slave Receiver status codes
 #define	TWI_SRX_ADR_ACK				0x60  // Own SLA+W has been received ACK has been returned
@@ -81,212 +32,155 @@ void flushTwiBuffers( void );
 #define	TWI_NO_STATE				0xF8  // No relevant state information available; TWINT = 0
 #define	TWI_BUS_ERROR				0x00  // Bus error due to an illegal START or STOP condition
 
-/* *** Local variables *** */
-
-static volatile uint8_t rxHead;
-static volatile uint8_t rxTail;
-static volatile uint8_t BufPointer;
-static uint8_t          rxBuf[ TWI_RX_BUFFER_SIZE ];
-static uint8_t          txBuf[ TWI_TX_BUFFER_SIZE ];
-static volatile uint8_t txHead;
-static volatile uint8_t txTail;
-
-/* *** Local Functions *** */
-/*
- * Reset TWI buffers pointers so that the FIFOs will show empty.
- */
-void
-flushTwiBuffers( void )
-{
-	rxTail = 0;
-	rxHead = 0;
-	txTail = 0;
-	txHead = TWI_TX_BUFFER_SIZE-1;
+void I2C_init(uint8_t address){
+	// load address into TWI address register
+	TWAR = (address << 1);
+	// set the TWCR to enable address matching and enable TWI, clear TWINT, enable TWI interrupt
+	TWCR = (1<<TWIE) | (1<<TWEA) | (1<<TWINT) | (1<<TWEN);
+	txbuffer[0]=0;
+	txbuffer[1]=1;
+	txbuffer[2]=2;
+	txbuffer[3]=3;
+	txbuffer[4]=4;
+	txbuffer[5]=5;
+	txbuffer[6]=6;
+	txbuffer[7]=7;
+	txbuffer[8]=8;
+	txbuffer[9]=9;
+	txbuffer[10]=10;
 }
 
-/* *** Public Functions *** */
-/*
- * Set up TWI hardware and set Slave I2C Address.
- * This is called once during the initialization process.
- * TODO: Allow GLOBAL address.
- */
-void
-twiSlaveInit( uint8_t adrs )
-{
-	TWAR = (adrs << 1);
-	
-	TWCR = (1<<TWEN)|(0<<TWIE)|(0<<TWINT)|(0<<TWEA)|(0<<TWSTA)|(0<<TWSTO)|(0<<TWWC);
-	return;
+void I2C_stop(void){
+	// clear acknowledge and enable bits
+	TWCR &= ~( (1<<TWEA) | (1<<TWEN) );
+}
+void I2C_writeRegister(uint8_t address,uint8_t dataL, uint8_t dataH){
+	//if(address<(0xFF-1)){
+	txbuffer[11]=dataL;
+	txbuffer[11+1]=dataH;
+	//}
 }
 
-/*
- * Enable I2C Slave
- * This is called to make the Slave ready to receive commands.
- * NOTE: Could set status flags here also.
- */
-void
-twiSlaveEnable( void )
-{
-	TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA)|(0<<TWSTA)|(0<<TWSTO)|(0<<TWWC);
-}
-	
-/*
- * Place data into the output buffer if there is available space.
- *
- * When a SDA_R is received, the data is automatically supplied
- * from the Transmit FIFO Buffer TxBuf[].
- * NOTE: Could set status flags here also or return a BUFFER_FULL on error.
- */
-void
-twiSetTxBuffer( uint8_t data[])
-{
-	for(int i=0;i<TWI_TX_BUFFER_SIZE;i++){
-		txBuf[i]=data[i];
-	}
-}
 
-/*
- * Read data from input buffer.
- * Data is automatically sent to the RxBuf[] FIFO when received from the Master.
- * Call twiDataInReceiveBuffer() and test for TRUE before calling this read function.
- *
- * Return 0x88 if no data is available. ERROR.
- */
-uint8_t*	
-twiGetRxBuffer( void )
-{
-	uint8_t* ret=rxBuf;
-	return ret;
-}
-
-/*
- * Check for available data in input buffer.
- * This function should return TRUE before calling twiReceiveByte() to get data.
- */
-bool
-twiDataInReceiveBuffer( void )
-{
-  // return 0 (false) if the receive buffer is empty
-  return rxHead != rxTail;
-}
-
-/*
- * Check that prior data was been sent.
- * This function should return FALSE when a Read request is received.
- */
-bool
-twiDataInTransmitBuffer( void )
-{
-  // return 0 (false) if the transmit buffer is empty
-  return txHead != txTail;
-}
-
-/*
- * Reset the output buffer to empty. Used to recover from sync errors.
- */
-void
-twiClearOutput( void )
-{
-	txTail = 0;
-	txHead = 0;
-}
-
-/*
- * Also used for manual input into input (rxBuf[]) buffer for testing.
- * NOTE: If RX buffer is full, data is lost.
- * TODO: ERROR condition on data lost.
- */
-void
-twiStuffRxBuf( uint8_t data )
-{
-	uint8_t tmphead;
-
-	// calculate buffer index
-	tmphead = ( rxHead + 1 ) & TWI_RX_BUFFER_MASK;
-
-	// check for free space in buffer
-	if ( tmphead == rxTail )
-	{
-		return;
-	}
-
-	// store data into buffer
-	rxBuf[ tmphead ] = data;
-
-	// update index
-	rxHead = tmphead;
-}
-
-/* *** Interrupt Service Routines *** */
-
-/*
- * TWI Interrupt Service
- * Called by TWI Event
- * This is a simple state machine that services a TWI Event. (see AVR311 for more detail)
- */
 ISR( TWI_vect )
 {
-	//debug
-	usart_putchar(TWSR);
-	switch( TWSR )
-	{
 		
-		case TWI_SRX_ADR_ACK:				// 0x60 Own SLA+W has been received ACK has been returned. Expect to receive data.
-//		case TWI_SRX_ADR_ACK_M_ARB_LOST:	// 0x68 Own SLA+W has been received; ACK has been returned. RESET interface.
-		flushTwiBuffers();
-			TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be DATA.
+		switch(TW_STATUS)
+		{
+			case TW_SR_SLA_ACK:
+			buffer_address = 0;
+			TWCR = (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
 			break;
-
-		case TWI_SRX_ADR_DATA_ACK:			// 0x80 Previously addressed with own SLA+W; Data received; ACK'd
-		case TWI_SRX_GEN_DATA_ACK:			// 0x90 Previously addressed with general call; Data received; ACK'd
-			// Put data into RX buffer
-			twiStuffRxBuf( TWDR );
-			TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be more DATA.
-  			break;
-			
-		case TWI_SRX_GEN_ACK:				// 0x70 General call address has been received; ACK has been returned
-//		case TWI_SRX_GEN_ACK_M_ARB_LOST:	// 0x78 General call address has been received; ACK has been returned
-			// TODO: Set General Address flag
-			TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be DATA.
+			case TW_SR_DATA_ACK:
+			// received data from master, call the receive callback
+			rxbuffer[buffer_address]=TWDR;
+			buffer_address++;
+			TWCR = (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
 			break;
-
-		case TWI_STX_ADR_ACK:				// 0xA8 Own SLA+R has been received; ACK has been returned. Load DATA.
-		flushTwiBuffers();
-//		case TWI_STX_ADR_ACK_M_ARB_LOST:	// 0xB0 Own SLA+R has been received; ACK has been returned
-		case TWI_STX_DATA_ACK:				// 0xB8 Data byte in TWDR has been transmitted; ACK has been received. Load DATA.
-			
-			if ( txHead != txTail )
-			{
-				txTail = ( txTail + 1 ) & TWI_TX_BUFFER_MASK;
-				TWDR = txBuf[ txTail ];
-			}
-			else
-			{
-				// the buffer is empty. Send 0x88. Too much data was asked for.
-				TWDR = 0x88;
-			}
-			TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event.
+			case TW_ST_SLA_ACK:
+			// master is requesting data, call the request callback
+			buffer_address = 0;
+			TWDR=txbuffer[buffer_address];
+			buffer_address++;
+			TWCR = (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
 			break;
-
-		case TWI_STX_DATA_NACK:				// 0xC0 Data byte in TWDR has been transmitted; NOT ACK has been received. End of Sending.
-			TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be new message.
+			case TW_ST_DATA_ACK:
+			// master is requesting data, call the request callback
+			TWDR=txbuffer[buffer_address];
+			buffer_address++;
+			TWCR = (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
 			break;
-
-		case TWI_SRX_STOP_RESTART:			// 0xA0 A STOP condition or repeated START condition has been received while still addressed as Slave
-			TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event.
+			case TW_BUS_ERROR:
+			// some sort of erroneous state, prepare TWI to be readdressed
+			TWCR = 0;
+			TWCR = (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
 			break;
-
-		case TWI_SRX_ADR_DATA_NACK:			// 0x88 Previously addressed with own SLA+W; data has been received; NOT ACK has been returned
-		case TWI_SRX_GEN_DATA_NACK:			// 0x98 Previously addressed with general call; data has been received; NOT ACK has been returned
-		case TWI_STX_DATA_ACK_LAST_BYTE:	// 0xC8 Last byte in TWDR has been transmitted (TWEA = 0); ACK has been received
-		case TWI_NO_STATE:					// 0xF8 No relevant state information available; TWINT = 0
-		case TWI_BUS_ERROR:					// 0x00 Bus error due to an illegal START or STOP condition
-			TWCR =   (1<<TWSTO)|(1<<TWINT);   // Recover from TWI_BUS_ERROR
-			// TODO: Set an ERROR flag to tell main to restart interface.
+			default:
+			TWCR = (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
 			break;
-
-		default:							// OOPS
-			TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be more DATA.
-			break;
-	}
+		}
+	//
+	//
+	//cli();
+	////debug
+	//uint8_t data;
+	//uint8_t state=TWSR;
+	//switch( state )
+	//{
+		//
+		//case TWI_SRX_ADR_ACK:				// 0x60 Own SLA+W has been received ACK has been returned. Expect to receive data.
+		////		case TWI_SRX_ADR_ACK_M_ARB_LOST:	// 0x68 Own SLA+W has been received; ACK has been returned. RESET interface.
+		//buffer_address = 0xFF;
+		//TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be DATA.
+		//break;
+//
+		//case TWI_SRX_ADR_DATA_ACK:			// 0x80 Previously addressed with own SLA+W; Data received; ACK'd
+		//case TWI_SRX_GEN_DATA_ACK:			// 0x90 Previously addressed with general call; Data received; ACK'd
+		//// Put data into RX buffer
+		//// save the received byte inside data
+		//data = TWDR;
+		//
+		//// check wether an address has already been transmitted or not
+		//if(buffer_address == 0xFF){
+			//
+		//buffer_address = data;
+		//}
+		//else{ // if a databyte has already been received
+		//
+		//// store the data at the current address
+		//rxbuffer[buffer_address] = data;
+		//
+		//// increment the buffer address
+		//buffer_address++;
+		//}
+		//TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be more DATA.
+		//break;
+		//
+		//case TWI_SRX_GEN_ACK:				// 0x70 General call address has been received; ACK has been returned
+		////		case TWI_SRX_GEN_ACK_M_ARB_LOST:	// 0x78 General call address has been received; ACK has been returned
+		//// TODO: Set General Address flag
+		//TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be DATA.
+		//break;
+//
+		//case TWI_STX_ADR_ACK:				// 0xA8 Own SLA+R has been received; ACK has been returned. Load DATA.
+		////		case TWI_STX_ADR_ACK_M_ARB_LOST:	// 0xB0 Own SLA+R has been received; ACK has been returned
+		//case TWI_STX_DATA_ACK:				// 0xB8 Data byte in TWDR has been transmitted; ACK has been received. Load DATA.
+		//// copy data from TWDR to the temporary memory
+		//data = TWDR;
+		//
+		//// if no buffer read address has been sent yet
+		//if( buffer_address == 0xFF ){
+			//buffer_address = data;
+		//}
+		//
+		//// copy the specified buffer address into the TWDR register for transmission
+		//TWDR =buffer_address;
+		//// increment buffer read address
+		//buffer_address++;
+		//TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event.
+		//break;
+//
+		//case TWI_STX_DATA_NACK:				// 0xC0 Data byte in TWDR has been transmitted; NOT ACK has been received. End of Sending.
+		//TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be new message.
+		//break;
+//
+		//case TWI_SRX_STOP_RESTART:			// 0xA0 A STOP condition or repeated START condition has been received while still addressed as Slave
+		//TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event.
+		//break;
+//
+		//case TWI_SRX_ADR_DATA_NACK:			// 0x88 Previously addressed with own SLA+W; data has been received; NOT ACK has been returned
+		//case TWI_SRX_GEN_DATA_NACK:			// 0x98 Previously addressed with general call; data has been received; NOT ACK has been returned
+		//case TWI_STX_DATA_ACK_LAST_BYTE:	// 0xC8 Last byte in TWDR has been transmitted (TWEA = 0); ACK has been received
+		//case TWI_NO_STATE:					// 0xF8 No relevant state information available; TWINT = 0
+		//case TWI_BUS_ERROR:					// 0x00 Bus error due to an illegal START or STOP condition
+		//TWCR =   (1<<TWSTO)|(1<<TWINT);   // Recover from TWI_BUS_ERROR
+		//// TODO: Set an ERROR flag to tell main to restart interface.
+		//break;
+//
+		//default:							// OOPS
+		//TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWINT)|(1<<TWEA);		// Prepare for next event. Should be more DATA.
+		//break;
+	//}
+	//sei();
+	////usart_putchar(state);
 }
